30f56e3bf2b0f7e9f2fbc24b40a3580628b46f91
lylzju <31461849+lylzju@users.noreply.github.com>
2020-08-19 22:50:09 +0800

    977. 有序数组的平方
    给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。

---
+++ cpp/num_977.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        for (int &i : A)
+        {
+            i *= i;
+        }
+        sort(A.begin(), A.end());
+        return A;
+    }
+};
